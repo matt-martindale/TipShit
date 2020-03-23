dd7f38757912fe55c1f638122407b141ec89fc3e
Matt Martindale <martindalematt18@yahoo.com>
2020-03-23 11:52:10 -0600

    Initial commit

---
+++ TipShit/TipShit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipShit
+//
+//  Created by Matthew Martindale on 3/23/20.
+//  Copyright © 2020 Matthew Martindale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
